fec33402d70535180cca4b3806a3ba7bb031f54b
Taylor Noble <zachnoble1993@gmail.com>
2024-01-06 11:39:04 -0500

    Using nvim-java to handle java configuration

--- nvim/.config/nvim/lua/core/lazy.lua
+++ nvim/.config/nvim/lua/core/lazy.lua
@@ -62,6 +62,29 @@ lazy.setup({
 
     -- Special Java LSP handling
     { "mfussenegger/nvim-jdtls" },
+    -- nvim-java
+    { 
+        'nvim-java/nvim-java',
+        dependencies = {
+            'nvim-java/lua-async-await',
+            'nvim-java/nvim-java-core',
+            'nvim-java/nvim-java-test',
+            'nvim-java/nvim-java-dap',
+            'MunifTanjim/nui.nvim',
+            'neovim/nvim-lspconfig',
+            'mfussenegger/nvim-dap',
+            {
+                'williamboman/mason.nvim',
+                opts = {
+                    registries = {
+                        'github:nvim-java/mason-registry',
+                        'github:mason-org/mason-registry',
+                    },
+                },
+            } 
+        },
+    },
+
     -- Sonarlint for Java
     { url = "https://www.gitlab.com/schrieveslaach/sonarlint.nvim" },
     -- Special C# Handling for Godot
